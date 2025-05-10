d704d58c6d7c8154473c8dc25896224db6cc6c19
codingking00 <danny.mengen.li@gmail.com>
2025-05-10 21:51:13 +1000

    first step done

---
+++ server.ts
@@ -0,0 +1,54 @@
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
+import { Socket } from "net";
+import next from "next";
+import { parse } from "url";
+import {WebSocketServer,WebSocket} from "ws";
+
+const nextApp = next({dev: process.env.NODE_ENV !== "production"});
+const handle= nextApp.getRequestHandler();
+const clients:Set<WebSocket> = new Set();
+
+nextApp.prepare().then(()=> {
+    const server: Server = createServer((req: IncomingMessage, res: ServerResponse)=> {
+        handle(req,res, parse(req.url || '', true));
+    })
+
+    const wss = new WebSocketServer({noServer:true});
+
+    wss.on('connection',(ws:WebSocket)=> {
+        clients.add(ws);
+        console.log('new client connected');
+
+        ws.on('message',(message:Buffer, isBinary:boolean)=> {
+            console.log(`message received: ${message}`)
+            clients.forEach(client=> {
+                if(client.readyState===WebSocket.OPEN && (message.toString()!==`{"event":"ping"}`)) {
+                    client.send(message,{binary:isBinary});
+                }
+            })
+        })
+        ws.on('close',()=> {
+            clients.delete(ws);
+            console.log('Client disconnected');
+        })
+    })
+
+    server.on("upgrade",(req:IncomingMessage,socket:Socket,head:Buffer)=> {
+        const {pathname} = parse(req.url || "/", true);
+
+        if(pathname==="/_next/webpack-hmr") {
+            nextApp.getUpgradeHandler()(req,socket,head);
+        }
+        if(pathname==="/api/ws") {
+            wss.handleUpgrade(req,socket,head, (ws)=> {
+                wss.emit('connection',ws,req);
+            })
+        }
+
+    })
+
+    server.listen(3000);
+    console.log('server listening on port 3000');
+    
+
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,123 @@
-import Image from "next/image";
+'use client'
+import { useEffect, useRef, useState } from "react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [messages, setMessages] = useState<String[]>([]);
+  // const [nameInput,setNameInput]= useState('')
+  // const [user,setUser] = useState('');
+  const [newMessage, setNewMessage] = useState('');
+  const [connectionStatus, setConnectionStatus] = useState<'connected' | 'disconnected' | 'connecting'>('connected');
+  const wsRef = useRef<WebSocket | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  useEffect(() => {
+      const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
+      const ws = new WebSocket(`${protocol}//${window.location.host}/api/ws`);
+      wsRef.current = ws;
+
+      ws.onopen = () => {
+          setConnectionStatus('connected');
+      }
+      ws.onclose = () => {
+          setConnectionStatus('disconnected');
+      }
+      ws.onmessage = (e) => {
+              setMessages((pre) => [...pre, e.data]);
+            
+      }
+      const pingInterval = setInterval(() => {
+          if (ws.readyState === WebSocket.OPEN) {
+              ws.send(`{"event":"ping"}`);
+          }
+      }, 29000)
+
+      return () => {
+          clearInterval(pingInterval);
+          if (wsRef.current) {
+              wsRef.current.close();
+          }
+      }
+  }, [])
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!newMessage.trim()) return;
+
+      if (wsRef.current?.readyState === WebSocket.OPEN) {
+          wsRef.current.send(newMessage);
+          setNewMessage('');
+      }
+  }
+
+  // if (!user) {
+  //     return (
+  //       <main className="h-screen flex items-center justify-center bg-gray-50">
+  //         <form
+  //           onSubmit={(e) => {
+  //             e.preventDefault();
+  //             if(nameInput.trim()) {
+  //                 setUser(nameInput)
+  //             }
+  //           }}
+  //           className="bg-white p-8 rounded shadow-lg space-y-4"
+  //         >
+  //           <h2 className="text-lg font-semibold text-gray-700">Enter your name</h2>
+  //           <input
+  //             type="text"
+  //             value={nameInput}
+  //             onChange={e=> {
+  //                 setNameInput(e.target.value)
+  //             }}
+  //             placeholder="Your name"
+  //             className="border px-4 py-2 rounded w-full"
+  //           />
+  //           <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded w-full">
+  //             Join Chat
+  //           </button>
+  //         </form>
+  //       </main>
+  //     );
+  //   }
+  return (
+          <main className="flex justify-center min-h-screen items-center bg-gradient-to-b from-gray-400 to-gray-100">
+              <div className="w-full max-w-2xl border border-gray-200 mx-4 bg-white rounded-xl shadow-lg flex flex-col h-[60vh] ">
+                  <div className={`px-6 py-4 text-sm font-bold rounded-t-xl ${
+                      connectionStatus==='connected'? 'bg-green-100 text-green-700 border-b border-green-200':
+                      connectionStatus==='disconnected'? 'bg-red-100 text-red-700 border-b border-red-200':
+                      'bg-yellow-100 text-yellow-700 border-b border-yellow-200'
+                  }`}>
+                      <div className="flex items-center gap-2">
+                          <div className={`w-2 h-2 rounded-full ${
+                              connectionStatus==='connected'? 'bg-green-500':
+                              connectionStatus==='disconnected'? 'bg-red-500':
+                              'bg-yellow-500 animate-ping'
+                          }`}></div>
+                          Status:{connectionStatus}
+                      </div>
+                  </div>
+                  <div className="flex-1 bg-gray-50 overflow-y-auto space-y-4">
+                      {messages.map((message,index)=> (
+                          <div key={index} className="bg-white p-2 rounded-lg  shadow-sm border border-gray-100 transition-all hover:shadow-md ">
+                              <p className=" font-medium text-shadow-amber-950">{message}</p>
+                          </div>
+                      ))}
+                  </div>
+                  <form onSubmit={sendMessage} className="border-t border-gray-100 p-3 bg-white rounded-b-xl">
+                      <div className="flex gap-3">
+                          <input type="text" value={newMessage} onChange={e=> {
+                              setNewMessage(e.target.value)
+                          }}
+                          className="flex-1 rounded-lg border border-gray-200 px-2 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-transparent transition-all"
+                          placeholder="Type your message" />
+                          <button type="submit" disabled={connectionStatus!=='connected'}
+                          className={`px-6 py-3 rounded-lg font-medium transition-all ${
+                              connectionStatus==='connected'? 'bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 shadow-sm hover:shadow':
+                              'bg-gray-300 text-gray-400 cursor-not-allowed'
+                          }`}>
+                              Send
+                          </button>
+                      </div>
+                  </form>
+              </div>
+          </main>
+  )
 }
